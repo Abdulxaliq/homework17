3dbd45bdcdbcd79e38bf5ae1e15ef4d35e304aa4
Abdulxaliq <Agaye_ck17@itstep.edu.az>
2024-08-27 14:50:24 +0400

    Add project files.

---
+++ homework17/homework17.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //const int size = 10;
+    //int array[size] = { };
+
+//   srand(time(0));
+
+//   cout << "Original array: ";
+//   for (int i = 0; i < size; ++i)
+//   {
+//       array[i] = rand() % 41 - 20;
+//       cout << array[i] << " ";
+//   }
+//   cout << endl;
+
+
+//   for (int i = 0; i < size; ++i) 
+//   {
+//       if (array[i] < 0) array[i] = -array[i];
+//   }
+
+//   cout << "Deyisdirilmis array: ";
+//   for (int i = 0; i < size; ++i) cout << array[i] << " ";
+//   cout << endl;
+//   return 0;
+
+
+
+    //const int size = 10;
+    //char ededler[size] = { };
+    //srand(time(0));
+    //cout << "Original array: ";
+    //for (int i = 0; i < size; ++i) 
+    //{
+    //    ededler[i] = 'A' + (rand() % 26);
+    //    cout << (int)ededler[i] << " ";
+    //}
+    //cout << endl;
+    //for (int i = 0; i < size; ++i)
+    //{
+    //    if (ededler[i] >= '0' && ededler[i] <= '9') ededler[i] = 'A' + (ededler[i] - '0');
+    //}
+
+    //cout << "Herfler: ";
+    //for (int i = 0; i < size; ++i)
+    //{
+    //    cout << ededler[i] << " ";
+    //}
+    //cout << endl;
+
+    //return 0;
+
+
+    //const int size = 10;
+    //int ededler[size];
+    //int N;
+    //for (int i = 0; i < size; ++i)
+    //{
+    //    ededler[i] = i;
+    //}
+
+    //cout << "Random array: ";
+    //for (int i = 0; i < size; ++i)
+    //{
+    //    cout << ededler[i] << " ";
+    //}
+    //cout << endl;
+
+    //cout << "N-i qeyd edin: ";
+    //cin >> N;
+
+    //N = N % size;
+
+
+    //if (N > 0)
+    //{
+    //    int temp[size];
+
+    //    for (int i = 0; i < size; ++i)
+    //    {
+    //        temp[(i + N) % size] = ededler[i];
+    //    }
+
+    //    for (int i = 0; i < size; ++i)
+    //    {
+    //        ededler[i] = temp[i];
+    //    }
+    //}
+
+    //cout << "Arrayi N qeder surusdurdukde ";
+    //for (int i = 0; i < size; ++i)
+    //{
+    //    cout << ededler[i] << " ";
+    //}
+    //cout << endl;
+
+    //return 0;
+
+
+
+
+
+    //const int size = 10;
+    //float array[size] = { 2.5, 3.0, 4.5, 1.0, 5.5, 6.0, 7.5, 8.0, 9.5, 10.0 };
+    //float kesr[size];
+    //float tam[size];
+
+    //int kesrIndex = 0;
+    //int tamIndex = 0;
+
+    //for (int i = 0; i < size; ++i) {
+    //    if (array[i] != int(array[i])) {
+    //       kesr[kesrIndex++] = array[i];
+    //    }
+    //    else {
+    //        tam[tamIndex++] = array[i];
+    //    }
+    //}
+
+    //int index = 0;
+    //for (int i = 0; i < kesrIndex; ++i) {
+    //    array[index++] = kesr[i];
+    //}
+    //for (int i = 0; i < tamIndex; ++i) {
+    //    array[index++] = tam[i];
+    //}
+
+    //cout << "ilk kesrin ve sonra tam ededin gelmesi: ";
+    //for (int i = 0; i < size; ++i) {
+    //    cout << array[i] << " ";
+    //}
+    //cout << endl;
+
+
+
+}
